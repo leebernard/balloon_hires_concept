5de83c6d201b11993d5a65585bd7747110fbf670
leebernard <ldbernard@asu.edu>
2024-10-08 16:22:13 -0700

    Started adding the spectrum model

--- spectrum_calcs.py
+++ spectrum_calcs.py
@@ -35,6 +35,17 @@ ax.plot(wl_water, cross_sections_water, label='H2O, 30 mb 1500 K')
 
 ax.set_xlabel('wavelength (um)')
 ax.set_ylabel('absorption cross-section')
+
+# partial fraction of water, according to Peter et al 2024
+log_f_h2o = -3.80  # log base 10
+
+p0 = 1
+mass_water = 18
+mass_h2 = 2.3
+water_ratio = 10**log_f_h2o
+mass_average = (1 - water_ratio)*mass_h2 + water_ratio*mass_water
+
+
 # smooth to R=40,000
 
 
