42164cb573e3b041ace400c6a1e7450e687e24a2
leebernard <ldbernard@asu.edu>
2024-10-17 12:21:43 -0700

    Filtered the data to 40000, using a gaussian.

--- signal_noise_calcs.py
+++ signal_noise_calcs.py
@@ -11,16 +11,29 @@ import matplotlib.pyplot as plt
 
 from scipy.ndimage import gaussian_filter
 
+# housekeeping variables
+resolution = 40000
+
+
 # open files
 gemini_trans_file = 'data/mktrans_zm_16_15.dat'
 mk_trans_data = np.loadtxt(gemini_trans_file)
 
+wl = mk_trans_data[:, 0]  # um
+raw_spectrum = mk_trans_data[:, 1]
 
+# filter the data down to the resolution, using a gaussian
+sigma = 1/2.355 * np.mean(wl)/np.mean(np.diff(wl)) * 1/resolution
 
+filtered_spectrum = gaussian_filter(raw_spectrum, sigma)
 
-
-
-
+# plot the results of the filter
+test_fig, test_ax = plt.subplots(figsize=(8, 6))
+test_ax.plot(wl, filtered_spectrum, label=f'R={resolution}')
+test_ax.plot(wl, raw_spectrum, label='Raw spectrum data', color='r', alpha=0.5)
+test_ax.set_xlabel('wavelength (um)')
+test_ax.set_ylabel('Fractional Transmission')
+test_ax.legend()
 
 
 
