d7e12f768ac3383c6f80fe9c0b491021551a8185
leebernard <ldbernard@asu.edu>
2024-10-17 13:01:40 -0700

    Started adding snr estimates.

--- signal_noise_calcs.py
+++ signal_noise_calcs.py
@@ -12,8 +12,11 @@ import matplotlib.pyplot as plt
 from scipy.ndimage import gaussian_filter
 
 # housekeeping variables
+debug=False
 resolution = 40000
-
+R_gem = 8.1/2  # m
+R_super = 0.5/2  # m
+R_giga = 1.35/2  # m
 
 # open files
 gemini_trans_file = 'data/mktrans_zm_16_15.dat'
@@ -27,15 +30,22 @@ sigma = 1/2.355 * np.mean(wl)/np.mean(np.diff(wl)) * 1/resolution
 
 filtered_spectrum = gaussian_filter(raw_spectrum, sigma)
 
-# plot the results of the filter
-test_fig, test_ax = plt.subplots(figsize=(8, 6))
-test_ax.plot(wl, filtered_spectrum, label=f'R={resolution}')
-test_ax.plot(wl, raw_spectrum, label='Raw spectrum data', color='r', alpha=0.5)
-test_ax.set_xlabel('wavelength (um)')
-test_ax.set_ylabel('Fractional Transmission')
-test_ax.legend()
+if debug:
+    # plot the results of the filter
+    test_fig, test_ax = plt.subplots(figsize=(8, 6))
+    test_ax.plot(wl, filtered_spectrum, label=f'R={resolution}')
+    test_ax.plot(wl, raw_spectrum, label='Raw spectrum data', color='r', alpha=0.5)
+    test_ax.set_xlabel('wavelength (um)')
+    test_ax.set_ylabel('Fractional Transmission')
+    test_ax.legend()
 
 
+# calculate SNR estimates
+planet_contrast = 1e-3
+snr_gem = 200
+oversample = 1/2 * filtered_spectrum.size / resolution
+exposure_time = 120  # s
 
-
+N_lines = np.sqrt(np.sum(1-filtered_spectrum)/3)
+snr_cc = snr_gem * np.sqrt(exposure_time/120) * R_gem/R_gem * planet_contrast * np.sqrt(N_lines)
 
