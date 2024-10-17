965b6be21ef88e1131856f2f7445fe1881d87054
leebernard <ldbernard@asu.edu>
2024-10-17 13:55:55 -0700

    Added the snr calculations for Superbit and Gigabit.

--- signal_noise_calcs.py
+++ signal_noise_calcs.py
@@ -43,16 +43,21 @@ if debug:
 
 
 # calculate SNR estimates
-usable_spectrum = (wl > 1.56) * (wl < 1.62)
-
 planet_contrast = 1e-3
 snr_gem = 200
 oversample = 1/2 * filtered_spectrum.size / resolution
 exposure_time = 3600 * 4.7  # s
 
-N_lines = np.sum(1-filtered_spectrum[usable_spectrum])/3
-snr_cc = snr_calculator(snr_gem, exposure_time, R_gem, planet_contrast, N_lines, r_ref=R_gem)
+usable_spectrum = (wl > 1.56) * (wl < 1.62)
+N_lines_gem = np.sum((1-filtered_spectrum[usable_spectrum])**2)
+snr_cc_gem = snr_calculator(snr_gem, exposure_time, R_gem, planet_contrast, N_lines_gem, r_ref=R_gem)
 
-print(f'SNR cc: {snr_cc: .2f}')
+print(f'SNR cc, Gemini: {snr_cc_gem: .2f}')
 
+usable_spectrum = (wl > 1.47) * (wl < 2.5)
+N_lines_super = np.sum((1-filtered_spectrum[usable_spectrum])**2)
+snr_cc_superbit = snr_calculator(snr_gem, exposure_time, R_super, planet_contrast, N_lines_super, r_ref=R_gem)
+print(f'SNR cc, Superbit platform: {snr_cc_superbit: .2f}')
 
+snr_cc_gigabit = snr_calculator(snr_gem, exposure_time, R_giga, planet_contrast, N_lines_super, r_ref=R_gem)
+print(f'SNR cc, Gigabit platform: {snr_cc_gigabit: .2f}')
