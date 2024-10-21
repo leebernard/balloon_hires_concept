e9c0410067c30b7188ebb8518ed11f5b50e2799e
leebernard <ldbernard@asu.edu>
2024-10-21 11:42:59 -0700

    Swapped to unfiltered spectrum data, in line with Nat's advice.

--- signal_noise_calcs.py
+++ signal_noise_calcs.py
@@ -41,38 +41,38 @@ if debug:
     # plot the results of the filter
     test_fig, test_ax = plt.subplots(figsize=(8, 6))
     test_ax.plot(wl, 1 - filtered_spectrum, label=f'R={resolution}')
-    # test_ax.plot(wl, 1-raw_spectrum, label='Raw spectrum data', color='r', alpha=0.5)
+    test_ax.plot(wl, 1-raw_spectrum, label='Raw spectrum data', color='r', alpha=0.3)
     test_ax.set_xlabel('wavelength (um)')
     test_ax.set_ylabel('Fractional Transparency')
-    test_ax.set_xlim(1.47, 2.5)
+    test_ax.set_xlim(1.597, 1.602)
     test_ax.legend()
 
 
 # calculate SNR estimates
 planet_contrast = 1e-3
 snr_gem = 200
-oversample = 1/2 * filtered_spectrum.size / resolution
+oversample = 1/2 * raw_spectrum.size / resolution
 exposure_time = 3600 * 4.7  # s
 
 usable_spectrum = (wl > 1.56) * (wl < 1.62)
-N_lines_gem = np.sum((1-filtered_spectrum[usable_spectrum])**2)
-# N_lines_gem = np.sum(1-filtered_spectrum[usable_spectrum])/3
+N_lines_gem = np.sum((1-raw_spectrum[usable_spectrum])**2)
+# N_lines_gem = np.sum(1-raw_spectrum[usable_spectrum])/3
 # N_lines_gem = ((1-raw_spectrum[usable_spectrum])**2).sum()   # 10.4
 snr_cc_gem = snr_calculator(snr_gem, exposure_time, R_gem, planet_contrast, N_lines_gem, r_ref=R_gem)
 
-print(f'SNR cc, Gemini: {snr_cc_gem: .2f}')
+print(f'SNR cc for CO, Gemini: {snr_cc_gem: .2f}')
 
 # full igrins spectrum
 hband = (wl > igrins_hband[0]) * (wl < igrins_hband[1])
 kband = (wl > igrins_kband[0]) * (wl < igrins_kband[1])
 
-N_lines_igrins = np.sum( (1 - filtered_spectrum[hband+kband]))
+N_lines_igrins = np.sum( (1 - raw_spectrum[hband+kband]))
 snr_cc_igrins = snr_calculator(snr_gem, exposure_time, R_gem, planet_contrast, N_lines_igrins, r_ref=R_gem)
 print(f'SNR cc, full igrins spectrum on Gemini: {snr_cc_igrins: .2f}')
 
 # superbit and gigabit platforms
 usable_spectrum = (wl > 1.47) * (wl < 2.5)
-N_lines_super = np.sum((1-filtered_spectrum[usable_spectrum])**2)
+N_lines_super = np.sum((1-raw_spectrum[usable_spectrum])**2)
 snr_cc_superbit = snr_calculator(snr_gem, exposure_time, R_super, planet_contrast, N_lines_super, r_ref=R_gem)
 print(f'SNR cc, Superbit platform: {snr_cc_superbit: .2f}')
 
